91d3c5a1977366419e270192e3bda3d13b30111b
hape <hapemartini@gmail.com>
2022-04-22 07:22:08 +0200

    add questions to the "better" solution and make it more understandable

--- better_1.js
+++ better_1.js
@@ -1,20 +1,26 @@
-function question(description, options, print) {
+function question(description, print) {
     console.log(description);
-    options.forEach((option, index) => {
-        console.log(`${index + 1}. ${option}`);
-    });
     print();
+    console.log('---------------');
 }
 
-function printQuiz(questions_good1) {
-    console.log('GOOD_1');
-    questions_good1.forEach(question => {
+function printQuiz(questions_good2) {
+    console.log('BETTER');
+    questions_good2.forEach(question => {
         console.log('');
     })
 }
 
 const questions_good2 = [
-    question('test', ['t1', 't2'], () => console.log('test test')),
+    question('How you doing?', () => console.log('Answer: _________')),
+    question('This statement is stateless', () => {
+        console.log('1. true');
+        console.log('2. false');
+    }),
+    question('Are you crazy?', () => ['yep', 'nope', 'maybe', 'WHAAAAT?!']
+        .forEach((option, index) => {
+            console.log(`${index + 1}. ${option}`);
+        })),
 ];
 
 printQuiz(questions_good2);
